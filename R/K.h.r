5c3d91f692f5a30f1f0b6bd21b14b4e287992db6
renatoagro <63155977+renatoagro@users.noreply.github.com>
2020-04-07 14:33:06 -0300

    Update K.h.r

--- R/K.h.r
+++ R/K.h.r
@@ -1,11 +1,9 @@
-K.h <- 
-function(Ks, alpha, n, h) 
-{     
-        m <-  1 - (1/n)
-        x <- (alpha*h)^(n-1)
-        y <- (1 + (alpha*h)^n)^-m
-        A <- (1 - x*y)^2
-        B <- (1 + (alpha*h)^n )^(m/2)
-        out <- Ks*(A/B)
-        return(out)
-}
+Kr <- 
+function(Ks,alpha, n, h) 
+{
+   out <- ((1 - ((alpha * h)^(n - 1))*
+          (1 + (alpha * h)^n)^(1/n - 1))^2) /
+          (1 + (alpha * h)^n)^((n-1)/(2 * n))*Ks
+   return(out)
+
+ }
