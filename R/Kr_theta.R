40702144c37c965c35790ca5fbe0f6d8e510602a
renatoagro <63155977+renatoagro@users.noreply.github.com>
2021-10-12 11:51:02 -0300

    Update Kr_theta.R

--- R/Kr_theta.R
+++ R/Kr_theta.R
@@ -1,5 +1,5 @@
 
-Kr_theta <- function(theta,thetaS,thetaR, alpha, n,Ks,f=0.5) {
+Kr_theta <- function(theta,thetaS,thetaR,n,Ks,f=0.5) {
    m <- 1-(1/n)
    Se <- (theta-thetaR)/(thetaS-thetaR)
    a <- (1-(1-Se^(1/m))^m)^2
