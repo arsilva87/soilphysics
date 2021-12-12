b5cdaa774dc5264d4489f6d544d255c6e7c29031
Anderson R da Silva <anderson.silva@ifgoiano.edu.br>
2021-12-12 16:04:14 +0100

    Add files via upload

--- R/WRC_App.R
+++ R/WRC_App.R
@@ -1,118 +1,132 @@
-findWRC = function(w, h) {
-   y = w; x = h
-   tS = max(y)
-   tR = min(y)
+findWRC = function(w, h,
+                   mod2fit = c(TRUE, TRUE, TRUE, TRUE))
+{
+  y = w; x = h
+  tS = max(y)
+  tR = min(y)
+  stopifnot(any(mod2fit == TRUE))
 
-   # Eqs
-   models = c("van Genuchten", "Brooks & Corey", "Groenevelt & Grant", "Dexter")
-   npar = c(2, 2, 3, 4)
-   eqVG = expression( tR + (tS - tR) * (1 + (a * x)^n)^(-(1 - 1/n)) )
-   eqBC = expression( ifelse(x < hb, tS, tR + (tS - tR) * (x/hb)^-lambda) )
-   eqGG = expression( k1 * (exp(-k0/6.653^n) - exp(-k0/x^n)) )
-   eqDE = expression( tR + a1 * exp(-x/p1) + a2 * exp(-x/p2) )
-   exps = c(eqVG, eqBC, eqGG, eqDE)
+  # Eqs
+  models = c("van Genuchten", "Brooks & Corey",
+             "Groenevelt & Grant", "Dexter")
+  npar = c(2, 2, 3, 4)
+  eqVG = expression( tR + (tS - tR)*(1 + (a * x)^n)^(-(1 - 1/n)) )
+  eqBC = expression( ifelse(x < hb, tS, tR + (tS - tR)*(x/hb)^-lambda) )
+  eqGG = expression( k1 * (exp(-k0/6.653^n) - exp(-k0/x^n)) )
+  eqDE = expression( tR + a1 * exp(-x/p1) + a2 * exp(-x/p2) )
+  exps = c(eqVG, eqBC, eqGG, eqDE)
 
-   # -logliks
-   nllVG = function(par) {
-      e = y - eval(eqVG, list(a = par[1], n = par[2]))
-      -sum(dnorm(e, 0, sd(e), log = T))
-   }
-   nllBC = function(par) {
-      e = y - eval(eqBC, list(hb = par[1], lambda = par[2]))
-      -sum(dnorm(e, 0, sd(e), log = T))
-   }
-   nllGG = function(par) {
-      e = y - eval(eqGG, list(k0 = par[1], k1 = par[2], n = par[3]))
-      -sum(dnorm(e, 0, sd(e), log = T))
-   }
-   nllDE = function(par) {
-      e = y - eval(eqDE, list(a1 = par[1], p1 = par[2],
-         a2 = par[3], p2 = par[4]))
-      -sum(dnorm(e, 0, sd(e), log = T))
-   }
+  # -logliks
+  nllVG = function(par) {
+    e = y - eval(eqVG, list(a = par[1], n = par[2]))
+    -sum(dnorm(e, 0, sd(e), log = T))
+  }
+  nllBC = function(par) {
+    e = y - eval(eqBC, list(hb = par[1], lambda = par[2]))
+    -sum(dnorm(e, 0, sd(e), log = T))
+  }
+  nllGG = function(par) {
+    e = y - eval(eqGG, list(k0 = par[1], k1 = par[2], n = par[3]))
+    -sum(dnorm(e, 0, sd(e), log = T))
+  }
+  nllDE = function(par) {
+    e = y - eval(eqDE, list(a1 = par[1], p1 = par[2],
+                            a2 = par[3], p2 = par[4]))
+    -sum(dnorm(e, 0, sd(e), log = T))
+  }
 
-   # starting values
-   grVG = expand.grid(a = seq(0.002, 0.2, len = 30),
-      n = seq(1, 4, len = 30))
-   grBC = expand.grid(hb = seq(0, 100, len = 50),
-      lambda = seq(0.1, 3, len = 30))
-   grGG = expand.grid(k0 = seq(1, 20, len = 30),
-      k1 = seq(0, 1, len = 30),
-      n = seq(1, 10, len = 20))
-   grDE = expand.grid(a1 = seq(0.01, 0.4, len = 12),
-      p1 = seq(1000, 10000, len = 20),
-      a2 = seq(0.01, 0.5, len = 12),
-      p2 = seq(10, 2000, len = 12))
-   criVG = apply(grVG, 1, nllVG)
-   criBC = apply(grBC, 1, nllBC)
-   criGG = apply(grGG, 1, nllGG)
-   criDE = apply(grDE, 1, nllDE)
-   oVG = which.min(criVG)
-   oBC = which.min(criBC)
-   oGG = which.min(criGG)
-   oDE = which.min(criDE)
+  # starting values
+  if (mod2fit[1]) {
+    grVG = expand.grid(a = seq(0.002, 0.2, len = 30),
+                       n = seq(1, 4, len = 30))
+    criVG = apply(grVG, 1, nllVG)
+    oVG = which.min(criVG)
+    mleVG = optim(par = grVG[oVG, ], nllVG, hessian = TRUE)
+  }
+  if (mod2fit[2]) {
+    grBC = expand.grid(hb = seq(0, 100, len = 50),
+                       lambda = seq(0.1, 3, len = 30))
+    criBC = apply(grBC, 1, nllBC)
+    oBC = which.min(criBC)
+    mleBC = optim(par = grBC[oBC, ], nllBC, hessian = TRUE)
+  }
+  if (mod2fit[3]) {
+    grGG = expand.grid(k0 = seq(1, 20, len = 30),
+                       k1 = seq(0, 1, len = 30),
+                       n = seq(1, 10, len = 20))
+    criGG = apply(grGG, 1, nllGG)
+    oGG = which.min(criGG)
+    mleGG = optim(par = grGG[oGG, ], nllGG, hessian = TRUE)
+  }
+  if (mod2fit[4]) {
+    grDE = expand.grid(a1 = seq(0.01, 0.4, len = 12),
+                       p1 = seq(1000, 10000, len = 20),
+                       a2 = seq(0.01, 0.5, len = 12),
+                       p2 = seq(10, 2000, len = 12))
+    criDE = apply(grDE, 1, nllDE)
+    oDE = which.min(criDE)
+    mleDE = optim(par = grDE[oDE, ], nllDE, hessian = TRUE)
+  }
 
-   # MLEs
-   mleVG = optim(par = grVG[oVG, ], nllVG, hessian = TRUE)
-   mleBC = optim(par = grBC[oBC, ], nllBC, hessian = TRUE)
-   mleGG = optim(par = grGG[oGG, ], nllGG, hessian = TRUE)
-   mleDE = optim(par = grDE[oDE, ], nllDE, hessian = TRUE)
-   pars = list(VG = mleVG$par, BC = mleBC$par, GG = mleGG$par, DE = mleDE$par)
+  pars = list(VG = if(mod2fit[1]) mleVG$par else NA,
+              BC = if(mod2fit[2]) mleBC$par else NA,
+              GG = if(mod2fit[3]) mleGG$par else NA,
+              DE = if(mod2fit[4]) mleDE$par else NA)
 
-   # Fitting criteria
-   aic = c(VG = 2 * mleVG$value + 2*2,
-      BC = 2 * mleBC$value + 2*2,
-      GG = 2 * mleGG$value + 2*3,
-      DE = 2 * mleDE$value + 2*4
-   )
-   std = list(VG = sqrt(diag(solve(mleVG$hessian))),
-      BC = sqrt(diag(solve(mleBC$hessian))),
-      GG = sqrt(diag(solve(mleGG$hessian))),
-      DE = sqrt(diag(solve(mleDE$hessian)))
-   )
-   eVG = y - eval(eqVG, as.list(mleVG$par))
-   eBC = y - eval(eqBC, as.list(mleBC$par))
-   eGG = y - eval(eqGG, as.list(mleGG$par))
-   eDE = y - eval(eqDE, as.list(mleDE$par))
-   r2 = c(VG = 1 - sum(eVG^2)/sum((y - mean(y))^2),
-      BC = 1 - sum(eBC^2)/sum((y - mean(y))^2),
-      GG = 1 - sum(eGG^2)/sum((y - mean(y))^2),
-      DE = 1 - sum(eDE^2)/sum((y - mean(y))^2)
-   )
-   mape = c(VG = 100 * mean(abs(eVG)/y),
-      BC = 100 * mean(abs(eBC)/y),
-      GG = 100 * mean(abs(eGG)/y),
-      DE = 100 * mean(abs(eDE)/y)
-   )
+  # Fitting criteria
+  aic = c(VG = if(mod2fit[1]) 2*mleVG$value + 2*2 else NA,
+          BC = if(mod2fit[2]) 2*mleBC$value + 2*2 else NA,
+          GG = if(mod2fit[3]) 2*mleGG$value + 2*3 else NA,
+          DE = if(mod2fit[4]) 2*mleDE$value + 2*4 else NA
+  )
+  std = list(VG = if(mod2fit[1]) sqrt(diag(solve(mleVG$hessian))) else NA,
+             BC = if(mod2fit[2]) sqrt(diag(solve(mleBC$hessian))) else NA,
+             GG = if(mod2fit[3]) sqrt(diag(solve(mleGG$hessian))) else NA,
+             DE = if(mod2fit[4]) sqrt(diag(solve(mleDE$hessian))) else NA
+  )
+  eVG = if(mod2fit[1]) y - eval(eqVG, as.list(mleVG$par)) else NA
+  eBC = if(mod2fit[2]) y - eval(eqBC, as.list(mleBC$par)) else NA
+  eGG = if(mod2fit[3]) y - eval(eqGG, as.list(mleGG$par)) else NA
+  eDE = if(mod2fit[4]) y - eval(eqDE, as.list(mleDE$par)) else NA
 
-   # out
-   dtf = data.frame(Rsq = r2, MAPE = mape, AIC = aic, npar)
-   best_id = which.min(dtf[, "AIC"])
-   best = paste("Best fit (AIC):", models[best_id])
+  r2 = c(VG = if(mod2fit[1]) 1 - sum(eVG^2)/sum((y - mean(y))^2) else NA,
+         BC = if(mod2fit[2]) 1 - sum(eBC^2)/sum((y - mean(y))^2) else NA,
+         GG = if(mod2fit[3]) 1 - sum(eGG^2)/sum((y - mean(y))^2) else NA,
+         DE = if(mod2fit[4]) 1 - sum(eDE^2)/sum((y - mean(y))^2) else NA
+  )
+  mape = c(VG = if(mod2fit[1]) 100*mean(abs(eVG)/y) else NA,
+           BC = if(mod2fit[2]) 100*mean(abs(eBC)/y) else NA,
+           GG = if(mod2fit[3]) 100*mean(abs(eGG)/y) else NA,
+           DE = if(mod2fit[4]) 100*mean(abs(eDE)/y) else NA
+  )
 
-   w_range = c(tR = tR, tS = tS)
-   pa <- c(pars[[best_id]], w_range)
-   pas <- as.list(pa)
-   eq <- exps[best_id]
-   for(j in 1:length(pas)) {
-     eq <- gsub(names(pas)[j], round(pas[[j]], 5), eq, fixed = TRUE)
-   }
+  # out
+  dtf = data.frame(Rsq = r2, MAPE = mape, AIC = aic, npar)
+  best_id = which.min(dtf[, "AIC"])
+  best = paste("Best fit (AIC):", models[best_id])
 
-   out = list(e = exps[best_id],
-      fitting_criteria = dtf,
-      best_fit = models[best_id], equation = eq,
-      par = pars[[best_id]], std = std[[best_id]],
-      w_range = w_range)
-   return(out)
+  w_range = c(tR = tR, tS = tS)
+  pa <- c(pars[[best_id]], w_range)
+  pas <- as.list(pa)
+  eq <- exps[best_id]
+  for(j in 1:length(pas)) {
+    eq <- gsub(names(pas)[j], round(pas[[j]], 5), eq, fixed = TRUE)
+  }
+
+  out = list(e = exps[best_id],
+             fitting_criteria = dtf[complete.cases(dtf), ],
+             best_fit = models[best_id], equation = eq,
+             par = pars[[best_id]], std = std[[best_id]],
+             w_range = w_range)
+  return(out)
 }
 
 # ---------------------------------------------------------------------
-ui_findWRC <- fluidPage(h3("Find the best-fit Water Retention Curve using maximum likelihood"),
-  theme = shinytheme("spacelab"),
+ui_findWRC <- fluidPage(h4("Find the best-fit Water Retention Curve using maximum likelihood"),
   windowTitle = "soilphysics | WRC",
   sidebarLayout(
-       sidebarPanel(width = 3,
-                    h4("Import the data"),
+       sidebarPanel(width = 4,
+                    h4("Import data"),
                     h6(tags$a(href="https://ce99d4d6-d4c5-48a3-b911-9e83247054ca.filesusr.com/ugd/45a659_170e68a8ee1f474b9493bc67df713eed.csv?dn=MyWRCExample.csv",
                               "(example)")),
                     fileInput("file", "(comma-separated values)",
@@ -120,16 +134,30 @@ ui_findWRC <- fluidPage(h3("Find the best-fit Water Retention Curve using maximu
                                        'text/comma-separated-values,text/plain',
                                        '.csv')
                     ),
-                    checkboxInput("semicolon", "Semicolon-separated"),
-                    checkboxInput("dec", "Comma as decimal separator"),
-                    selectInput('w', 'Water content (y)', ''),
-                    selectInput('h', 'Matric potential (x)', ''),
+                    fluidRow(
+                      column(6, checkboxInput("semicolon", "Semicolon-separated")),
+                      column(6, checkboxInput("dec", "Comma as decimal"))
+                    ),
+                    fluidRow(
+                    column(6, selectInput('w', 'Water content (y)', '')),
+                    column(6, selectInput('h', 'Matric potential (x)', ''))
+                    ),
                     checkboxInput("logh", "Log10( x )"),
-                    actionButton("run", "Run", icon = icon("r-project"))
+                    fluidRow(
+                      h4("Models to fit:"),
+                      column(6, checkboxInput("modVG", "van Genuchten", value = TRUE)),
+                      column(6, checkboxInput("modBC", "Brooks & Corey", value = TRUE)),
+                      column(6, checkboxInput("modGG", "Groenevelt & Grant", value = TRUE)),
+                      column(6, checkboxInput("modDE", "Dexter", value = TRUE))
+                    ),
+                    actionButton("run", "RUN", width = "40%",
+                                 icon = icon("r-project"),
+                                 style="color: #fff; background-color: #337ab7; border-color: #2e6da4")
              ),
              mainPanel(
                tabsetPanel(tabPanel("Data", icon = icon("table"),
-                                   h5("Input data (editable)"),
+                                   h5("Input data (editable, left-click)
+                                   (right-click to insert or remove rows)"),
                                    rHandsontableOutput("tab")
                           ),
                           tabPanel("Results", icon = icon("calculator"),
@@ -210,7 +238,9 @@ server_findWRC <- function(session, input, output) {
     if (input$logh) {
       xy[,1] <- log10(xy[,1])
     }
-    r <- findWRC(w = xy[,2], h = xy[,1])
+    r <- findWRC(w = xy[,2], h = xy[,1],
+                 mod2fit = c(input$modVG, input$modBC,
+                             input$modGG, input$modDE))
     output$fit <- renderPrint( r[-1] )
     output$graph <- renderPlot({
       par(mar = c(4.5, 4.5, 2.5, 1), las = 1, cex = 1.3)
